ea0d0d7d2938b7acc6f697e4971b97229a95cd6e
Dmitrii Iurin <dimon07710@yandex.ru>
2019-11-02 20:09:15 +0300

    First version

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    logger.info "Subscribed to RoomChannel, roomId: #{params[:roomId]}"
+
+    @room = Room.find(params[:roomId])
+
+    stream_from "room_channel_#{@room.id}"
+
+    speak('message' => '* * * joined the room * * *')
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+    logger.info 'Unsubscribed to RoomChannel'
+
+    speak('message' => '* * * left the room * * *')
+  end
+
+  def speak(data)
+    logger.info "RoomChannel, speak: #{data.inspect}"
+
+    MessageService.new(
+      body: data['message'], room: @room, user: current_user
+    ).perform
+  end
+end
---
+++ app/services/message_service.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class MessageService
+  def initialize(body:, user:, room:)
+    @body = body
+    @user = user
+    @room = room
+  end
+
+  def perform
+    create_message!
+    broadcast_message
+  end
+
+  private
+
+  def create_message!
+    @message ||= Message.create! body: @body, room: @room, user: @user
+  end
+
+  def broadcast_message
+    ActionCable.server.broadcast "room_channel_#{@room.id}",
+                                 message: render_message
+  end
+
+  def render_message
+    ApplicationController.renderer.render(partial: 'messages/message', locals: {
+                                            message: @message
+                                          })
+  end
+end
